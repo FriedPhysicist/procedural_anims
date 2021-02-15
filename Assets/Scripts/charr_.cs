18ccc99a5c0cea06f4acb6ab9f6090cc7cbc7bc9
FriedPhysicist <59540097+FriedPhysicist@users.noreply.github.com>
2021-02-15 23:07:42 +0300

    GF

---
+++ Assets/Scripts/charr_.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charr_ : MonoBehaviour
+{
+    public Transform spine;
+    private float x;
+    private float y;
+    public float sens;
+    
+    void Start()
+    { 
+    }
+
+    
+    void LateUpdate()
+    { 
+        fps_camera_movement.procedural_anims.spine_move(spine,transform,ref x, ref y,sens);
+        //Debug.Log(x+" "+y);
+    }
+}
